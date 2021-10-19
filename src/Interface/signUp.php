90289154e5ac01988634f1c77b58629e7d5f1935
GeoffreyPorayko <you@example.com>
2021-10-19 13:59:14 +0200

    update

---
+++ src/Interface/signUp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="/user" method="post">
+      <label for="name">Name: </label>
+      <input type="text" name="name" value="">
+      <label for="login">Login: </label>
+      <input type="text" name="login" value="">
+      <label for="password">Password: </label>
+      <input type="text" name="password" value="">
+      <input type="submit" name="submit" value="Submit">
+    </form>
+  </body>
+</html>
