0bec15e50ad81d9e35a48ce4bc71e27e84234104
GeoffreyPorayko <geoffrey.poraykostexp7@gmail.com>
2021-10-31 19:44:28 +0100

    localisation fini

--- src/Interface/index.php
+++ src/Interface/index.php
@@ -8,6 +8,9 @@
 
  <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
 	<script type="text/javascript">
+    var listeMarker = []
+    var mapInit=0
+    var macarte = null;
     function success(position) {
       const latitude  = position.coords.latitude;
       const longitude = position.coords.longitude;
@@ -19,45 +22,98 @@
         longitude: longitude
       }
     }).done(function(data) {
-      console.log("ok")
     });
 
-      var macarte = null;
-      window.onload = function(){
-		  initMap(); 
-            };
-            
+		  if(mapInit == 0){
+        initMap();
+       }else{
+         markerUpdate()
+       } 
+       mapInit=1    
        // Fonction d'initialisation de la carte
        function initMap() {
+         for(i=0;i<listeMarker.length;i++){
+          macarte.removeLayer(listeMarker[i]);
+         }
            // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
            macarte = L.map('map').setView([latitude, longitude], 16);
            // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
            L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
                // Il est toujours bien de laisser le lien vers la source des données
                attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-               minZoom: 10,
+               minZoom: 14,
                maxZoom: 20
            }).addTo(macarte);
-           var marker = L.marker([latitude, longitude]).addTo(macarte);
+           var meIcon = L.icon({
+            iconUrl: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+            iconSize:     [40, 40], // size of the icon
+            popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
+          });
+           var marker = L.marker([latitude, longitude],{icon: meIcon}).addTo(macarte);
+           marker.bindPopup("Moi");
+           listeMarker.push(marker)
            $.get( "http://localhost:8080/coord", function( data ) {
-             var response = JSON.parse(data)
-            console.log(JSON.parse(data)[0].prenom);
+             var response = JSON.parse(data);
+            for(i=0;i<response.length;i++){
+              if(response[i].latitude <= latitude+0.05 && response[i].latitude >= latitude-0.05 && response[i].longitude <= longitude+0.05 && response[i].longitude >= longitude-0.05){
+                var marker = L.marker([response[i].latitude, response[i].longitude]).addTo(macarte)
+                if(response[i].anonyme == 0){
+                  marker.bindPopup(response[i].prenom+" "+response[i].nom);
+                }else marker.bindPopup('Cette personne souhaite rester anonyme');
+                listeMarker.push(marker)
+              }
+            }
             
             });
 
       status.textContent = '';
+      }
+
+      function markerUpdate() {
+         for(i=0;i<listeMarker.length;i++){
+          macarte.removeLayer(listeMarker[i]);
+         }
+           var meIcon = L.icon({
+            iconUrl: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+            iconSize:     [40, 40], // size of the icon
+            popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
+          });
+           var marker = L.marker([latitude, longitude],{icon: meIcon}).addTo(macarte);
+           marker.bindPopup("Moi");
+           listeMarker.push(marker)
+           $.get( "http://localhost:8080/coord", function( data ) {
+             var response = JSON.parse(data);
+            for(i=0;i<response.length;i++){
+              if(response[i].latitude <= latitude+0.05 && response[i].latitude >= latitude-0.05 && response[i].longitude <= longitude+0.05 && response[i].longitude >= longitude-0.05){
+                var marker = L.marker([response[i].latitude, response[i].longitude]).addTo(macarte)
+                if(response[i].anonyme == 0){
+                  marker.bindPopup(response[i].prenom+" "+response[i].nom);
+                }else marker.bindPopup('Cette personne souhaite rester anonyme');
+                listeMarker.push(marker)
+              }
+            }
+            
+            });
+
+      status.textContent = '';
+      }
     }
 
     function error() {
       status.textContent = 'Unable to retrieve your location';
     }
 
+    function localisation(){
     if (!navigator.geolocation) {
       status.textContent = 'Geolocation is not supported by your browser';
     } else {
       status.textContent = 'Locating…';
       navigator.geolocation.getCurrentPosition(success, error);
+      }
     }
+
+    localisation()
+    setInterval(localisation,60000)
         </script>
   </body>
 </html>
